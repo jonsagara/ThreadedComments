b589b873957887ccd5e83afd04f3b46ffeb590b9
Jon Sagara <jon@sagarasoftware.com>
2012-11-20 15:57:01 -0800

    Got static sorting by SortOrder to work. Now must figure out how to get it to work with a karma/vote score.

--- src/ThreadedComments/Controllers/PostController.cs
+++ src/ThreadedComments/Controllers/PostController.cs
@@ -28,7 +28,6 @@ namespace ThreadedComments.Controllers
             var comments = ThreadedCommentsContext.Comments
                 .Where(c => c.PostId == id && c.BoardId == post.BoardId)
                 .OrderBy(c => c.SortOrder)
-                .ThenBy(c => c.CommentUtcDate)
                 .Take(50)
                 .ToList()
                 .Select(c => new PostIndex.CommentInfo
@@ -54,7 +53,7 @@ namespace ThreadedComments.Controllers
             return View(viewModel);
         }
 
-        public ActionResult NewComment(int? id)
+        public ActionResult NewComment(int? id, int? parentCommentId)
         {
             var post = ThreadedCommentsContext.Posts
                 .Include("Board")
@@ -65,27 +64,106 @@ namespace ThreadedComments.Controllers
                 throw new Exception("No Post with id = " + id);
             }
 
-            return View(new PostNewComment { PostId = post.PostId, BoardId = post.BoardId });
+            if (parentCommentId != null)
+            {
+                var parentComment = ThreadedCommentsContext.Comments.SingleOrDefault(c => c.CommentId == parentCommentId);
+                if (parentComment == null)
+                {
+                    throw new Exception("No parent Comment with id = " + parentCommentId);
+                }
+            }
+
+            return View(new PostNewComment { PostId = post.PostId, BoardId = post.BoardId, ParentCommentId = parentCommentId });
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult NewComment(PostNewComment viewModel)
         {
+            const string StartingParentLevelSortOrder = "9999999";
+            const string StartingChildLevelSortOrder = "0000001";
+
+            var post = ThreadedCommentsContext.Posts
+                .Include("Board")
+                .SingleOrDefault(p => p.PostId == viewModel.PostId);
+
+            if (post == null)
+            {
+                throw new Exception("No Post with id = " + viewModel.PostId);
+            }
+
+            Comment parentComment = null;
+            if (viewModel.ParentCommentId != null)
+            {
+                parentComment = ThreadedCommentsContext.Comments.SingleOrDefault(c => c.CommentId == viewModel.ParentCommentId);
+                if (parentComment == null)
+                {
+                    throw new Exception("No parent Comment with id = " + viewModel.ParentCommentId);
+                }
+            }
+
             if (ModelState.IsValid)
             {
                 var comment = new Comment();
                 comment.PostId = viewModel.PostId;
                 comment.BoardId = viewModel.BoardId;
-                comment.ParentCommentId = null;//viewModel.ParentCommentId;
+                comment.ParentCommentId = viewModel.ParentCommentId;
                 comment.Body = viewModel.Body.Trim();
                 comment.AuthorName = viewModel.AuthorName.Trim();
                 comment.AuthorEmail = viewModel.AuthorEmail.Trim();
                 comment.CommentUtcDate = DateTime.UtcNow;
 
-                comment.SortOrder = "0000001";
-#warning hard coded for now
-                comment.Indent = 0;
+                if (parentComment != null)
+                {
+                    // See if this parent has other comments that are siblings of this new comment. 
+                    //  If so, we'll use the latest one to set our sort order.
+                    var otherChildComments = ThreadedCommentsContext.Comments
+                        .Where(c => c.PostId == viewModel.PostId && c.BoardId == viewModel.BoardId && c.ParentCommentId == parentComment.CommentId)
+                        .OrderByDescending(c => c.SortOrder)
+                        .Take(1)
+                        .ToList();
+
+                    string sortOrder = StartingChildLevelSortOrder;
+                    if (otherChildComments.Count > 0)
+                    {
+                        var lastChildComment = otherChildComments.Single();
+                        var lastChildCommentSortOrderParts = lastChildComment.SortOrder.Split(new char[] {'.'}, StringSplitOptions.RemoveEmptyEntries);
+                        if (lastChildCommentSortOrderParts.Length == 0)
+                        {
+                            throw new Exception("Parent comment with id = " + lastChildComment.CommentId + " has an invalid SortOrder = " + lastChildComment.SortOrder);
+                        }
+
+                        sortOrder = (Convert.ToInt32(lastChildCommentSortOrderParts.Last()) + 1).ToString("0000000");
+                    }
+
+                    comment.SortOrder = parentComment.SortOrder + "." + sortOrder;
+                    comment.Indent = parentComment.Indent + 1;
+                }
+                else
+                {
+                    // See if this Post has other top-level comments that are siblings of this new
+                    //  comment. If so, we'll use the latest one to set our sort order.
+                    var otherTopLevelComments = ThreadedCommentsContext.Comments
+                        .Where(c => c.PostId == viewModel.PostId && c.BoardId == viewModel.BoardId && c.ParentCommentId == null)
+                        .OrderBy(c => c.SortOrder)
+                        .Take(1)
+                        .ToList();
+
+                    string sortOrder = StartingParentLevelSortOrder;
+                    if (otherTopLevelComments.Count > 0)
+                    {
+                        var lastTopLevelComment = otherTopLevelComments.Single();
+                        if (lastTopLevelComment.SortOrder.Length != 7)
+                        {
+                            throw new Exception("Top-level comment with id = " + lastTopLevelComment.CommentId + " has an invalid SortOrder = " + lastTopLevelComment.SortOrder + ". It should only consist of 7 integer characters.");
+                        }
+
+                        sortOrder = (Convert.ToInt32(lastTopLevelComment.SortOrder) - 1).ToString("0000000");
+                    }
+
+                    comment.SortOrder = sortOrder;
+                    comment.Indent = 0;
+                }
 
                 ThreadedCommentsContext.Comments.Add(comment);
 
